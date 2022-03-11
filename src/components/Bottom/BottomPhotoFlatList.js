eb8d3a63e9d58ed58fd2dd2039f162f5b8133eba
LeeHoSeop <llss2ssll@gmail.com>
2022-03-11 15:13:03 +0900

    바텀 에니메이션 추가

--- src/components/Bottom/BottomPhotoFlatList.js
+++ src/components/Bottom/BottomPhotoFlatList.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   FlatList,
   View,
@@ -10,7 +10,11 @@ import {
   Modal,
 } from 'react-native';
 import ModalAllPhotoFlatList from './ModalAllPhotoFlatList';
-
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 const BottomPhotoFlatList = props => {
   const [isModal, setIsModal] = useState(false);
 
@@ -53,166 +57,176 @@ const BottomPhotoFlatList = props => {
       }
     });
   };
+  let image_ref = useSharedValue({height: 0});
 
+  useEffect(() => {
+    if (!open) {
+      image_ref.value = {height: 0};
+    } else {
+      image_ref.value = {height: 320};
+    }
+  }, [open]);
+  const animtionStyle = useAnimatedStyle(() => {
+    return {
+      height: withTiming(image_ref.value.height, {
+        duration: 500,
+      }),
+    };
+  });
   return (
     <>
-      {open ? (
-        <View style={{height: 'auto', backgroundColor: '#fff'}}>
-          <View
-            style={{
-              width: '100%',
-              flexDirection: 'row',
-              height: 40,
-            }}>
-            <TouchableOpacity
-              onPress={async () => {
-                await setPick_count(group_name[0].count);
-                await setTimeout(() => {
-                  setPick_group(group_name[0].title);
-                }, 500);
-                setChoise_imasge(final_image);
-                setOpen(false);
-              }}
-              style={{
-                width: 40,
-                height: 40,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <Image
-                style={{width: 20, height: 20}}
-                source={require('../../assets/images/close_icon.png')}
-              />
-            </TouchableOpacity>
-            <View style={{flex: 1, alignItems: 'flex-end'}}>
-              {choise_imasge.length !== 0 && (
-                <TouchableOpacity
-                  onPress={async () => {
-                    setFinal_image(choise_imasge);
-                    await setPick_count(group_name[0].count);
-                    await setTimeout(() => {
-                      setPick_group(group_name[0].title);
-                    }, 500);
-                    setOpen(false);
-                  }}
-                  style={{
-                    width: 40,
-                    height: 40,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    backgroundColor: 'skyblue',
-                  }}>
-                  <Image
-                    style={{width: 20, height: 20}}
-                    source={require('../../assets/images/send_icon.png')}
-                  />
-                </TouchableOpacity>
-              )}
-            </View>
-          </View>
-          <FlatList
-            scrollEnabled={loading ? !loading : true}
-            horizontal
-            data={data}
-            keyExtractor={(item, index) => index.toString()}
-            onEndReachedThreshold={0.3}
-            showsHorizontalScrollIndicator={false}
-            renderItem={({item, index}) => {
-              return CustomRenderItem ? (
-                <CustomRenderItem item={item} index={index} />
-              ) : (
-                <TouchableOpacity
-                  onPress={() => {
-                    if (someArray(item.node.image.uri) === true) {
-                      setChoise_imasge(
-                        choise_imasge.filter(data => {
-                          return data.uri !== item.node.image.uri;
-                        }),
-                      );
-                    } else {
-                      setChoise_imasge(data => {
-                        return [
-                          ...data,
-                          {
-                            uri: item.node.image.uri,
-                            type: 'image',
-                            name: item.node.image.filename,
-                          },
-                        ];
-                      });
-                    }
-                  }}
-                  style={[
-                    styles.ImageChoiseView,
-                    {
-                      borderWidth: 2,
-                      borderColor: someArray(item.node.image.uri)
-                        ? 'skyblue'
-                        : '#fff',
-                    },
-                  ]}>
-                  <Image
-                    style={[styles.ImageStyle]}
-                    source={{
-                      uri: item.node.image.uri,
-                    }}
-                  />
-                  <View
-                    style={[
-                      styles.ImageChoiseButtonView,
-                      someArray(item.node.image.uri) && {
-                        backgroundColor: 'skyblue',
-                        borderColor: 'skyblue',
-                      },
-                    ]}>
-                    {choise_imasge?.length !== 0 &&
-                      choise_imasge.find(
-                        x => x.uri === item.node.image.uri,
-                      ) && (
-                        <Text style={[styles.FontChoiseButtonStyle]}>
-                          {choise_imasge?.findIndex(
-                            obj => obj?.uri == item.node.image.uri,
-                          ) + 1 ?? ''}
-                        </Text>
-                      )}
-                  </View>
-                </TouchableOpacity>
-              );
+      <Animated.View
+        style={[animtionStyle, {height: 'auto', backgroundColor: '#fff'}]}>
+        <View
+          style={{
+            width: '100%',
+            flexDirection: 'row',
+            height: 40,
+          }}>
+          <TouchableOpacity
+            onPress={async () => {
+              await setPick_count(group_name[0].count);
+              await setTimeout(() => {
+                setPick_group(group_name[0].title);
+              }, 500);
+              setChoise_imasge(final_image);
+              setOpen(false);
             }}
-            contentContainerStyle={styles.contentContainer}
-          />
-          <View
             style={{
-              width: '100%',
-              flexDirection: 'row',
-              paddingHorizontal: 10,
-              paddingVertical: 5,
+              width: 40,
+              height: 40,
+              justifyContent: 'center',
+              alignItems: 'center',
             }}>
-            <TouchableOpacity
-              onPress={() => {
-                setOpen(false);
-                setIsModal(true);
-              }}
-              style={{
-                width: 'auto',
-                height: 20,
-                flexDirection: 'row',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <Image
-                style={{width: 20, height: 20}}
-                source={require('../../assets/images/all_icon.png')}
-              />
-              <Text style={{fontSize: 15, fontWeight: '600', marginLeft: 5}}>
-                전체
-              </Text>
-            </TouchableOpacity>
+            <Image
+              style={{width: 20, height: 20}}
+              source={require('../../assets/images/close_icon.png')}
+            />
+          </TouchableOpacity>
+          <View style={{flex: 1, alignItems: 'flex-end'}}>
+            {choise_imasge.length !== 0 && (
+              <TouchableOpacity
+                onPress={async () => {
+                  setFinal_image(choise_imasge);
+                  await setPick_count(group_name[0].count);
+                  await setTimeout(() => {
+                    setPick_group(group_name[0].title);
+                  }, 500);
+                  setOpen(false);
+                }}
+                style={{
+                  width: 40,
+                  height: 40,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: 'skyblue',
+                }}>
+                <Image
+                  style={{width: 20, height: 20}}
+                  source={require('../../assets/images/send_icon.png')}
+                />
+              </TouchableOpacity>
+            )}
           </View>
         </View>
-      ) : (
-        <></>
-      )}
+        <FlatList
+          scrollEnabled={loading ? !loading : true}
+          horizontal
+          data={data}
+          keyExtractor={(item, index) => index.toString()}
+          onEndReachedThreshold={0.3}
+          showsHorizontalScrollIndicator={false}
+          renderItem={({item, index}) => {
+            return CustomRenderItem ? (
+              <CustomRenderItem item={item} index={index} />
+            ) : (
+              <TouchableOpacity
+                onPress={() => {
+                  if (someArray(item.node.image.uri) === true) {
+                    setChoise_imasge(
+                      choise_imasge.filter(data => {
+                        return data.uri !== item.node.image.uri;
+                      }),
+                    );
+                  } else {
+                    setChoise_imasge(data => {
+                      return [
+                        ...data,
+                        {
+                          uri: item.node.image.uri,
+                          type: 'image',
+                          name: item.node.image.filename,
+                        },
+                      ];
+                    });
+                  }
+                }}
+                style={[
+                  styles.ImageChoiseView,
+                  {
+                    borderWidth: 2,
+                    borderColor: someArray(item.node.image.uri)
+                      ? 'skyblue'
+                      : '#fff',
+                  },
+                ]}>
+                <Image
+                  style={[styles.ImageStyle]}
+                  source={{
+                    uri: item.node.image.uri,
+                  }}
+                />
+                <View
+                  style={[
+                    styles.ImageChoiseButtonView,
+                    someArray(item.node.image.uri) && {
+                      backgroundColor: 'skyblue',
+                      borderColor: 'skyblue',
+                    },
+                  ]}>
+                  {choise_imasge?.length !== 0 &&
+                    choise_imasge.find(x => x.uri === item.node.image.uri) && (
+                      <Text style={[styles.FontChoiseButtonStyle]}>
+                        {choise_imasge?.findIndex(
+                          obj => obj?.uri == item.node.image.uri,
+                        ) + 1 ?? ''}
+                      </Text>
+                    )}
+                </View>
+              </TouchableOpacity>
+            );
+          }}
+          contentContainerStyle={styles.contentContainer}
+        />
+        <View
+          style={{
+            width: '100%',
+            flexDirection: 'row',
+            paddingHorizontal: 10,
+            paddingVertical: 5,
+          }}>
+          <TouchableOpacity
+            onPress={() => {
+              setOpen(false);
+              setIsModal(true);
+            }}
+            style={{
+              width: 'auto',
+              height: 20,
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Image
+              style={{width: 20, height: 20}}
+              source={require('../../assets/images/all_icon.png')}
+            />
+            <Text style={{fontSize: 15, fontWeight: '600', marginLeft: 5}}>
+              전체
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </Animated.View>
       <Modal
         visible={isModal}
         transparent
